d51f0ec6cd49009f1c2f954fdc32c1ba212b203d
DaphnexxEliza <wanatchaya.idea@gmail.com>
2022-08-03 22:32:34 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,65 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventListView from '../views/EventListView.vue'
+import AboutView from '../views/AboutView.vue'
+import EventDetails from '@/views/event/EventDetailView.vue'
+import EventRegister from '@/views/event/EventRegister.vue'
+import EventEdit from '@/views/event/EditView.vue'
+import Eventlayout from '@/views/event/EventLayoutView.vue'
+const routes = [{
+        path: '/',
+        name: 'EventList',
+        component: EventListView,
+        props: (route) => ({
+            page: parseInt(route.query.page) || 1,
+            morepage: parseInt(route.query.morepage) || 1
+        })
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: AboutView
+    },
+    {
+        path: '/event/:id',
+        name: 'EventLayout',
+        props: true,
+        component: Eventlayout,
+        children: [{
+                path: '',
+                name: 'EventDetails',
+                component: EventDetails
+            },
+            {
+                path: 'register',
+                name: 'EventRegister',
+                props: true,
+                component: EventRegister
+            },
+            {
+                path: 'edit',
+                name: 'EventEdit',
+                props: true,
+                component: EventEdit
+            }
+        ]
+    },
+    {
+        path: '/event/:id/register',
+        name: 'EventRegister',
+        props: true,
+        component: EventRegister
+    },
+    {
+        path: '/event/:id/edit',
+        name: 'EventEdit',
+        props: true,
+        component: EventEdit
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router
---
+++ src/views/EventDetailView.vue
@@ -0,0 +1,32 @@
+<template>
+<div v-if="event">
+    <h1>{{event.title}}</h1>
+    <p>{{event.time}} on {{event.date}} @ {{event.location}}</p>
+    <p> {{event.description}}</p>
+</div>
+</template>
+<script>
+import EventService from '@/services/EventService'
+// import { create } from 'domain'
+// import { response } from 'express'
+
+export default{
+    prop: ['id'],
+    data(){
+        return{
+            event: null,
+            
+        }
+    },
+    create(){
+        EventService.getEvent (this.id)
+        .then(response => {
+            this.event = response.data
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+
+    }
+}
+</script>
